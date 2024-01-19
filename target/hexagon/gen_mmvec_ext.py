57e3f9956a24ba7a17e7dacdbf1a6b4560477786
Matheus Tavares Bernardino <quic_mathbern@quicinc.com>
2024-01-19 10:53:32 -0300

    target/hexagon: use different HVX semantics among revs
    When importing v79 HVX, we ended up breaking the simulation for v75
    binaries in QEMU as the semantics changed in some of the instructions,
    specially for QFloat, making the output values from v75 and v79 differ.
    So QEMU can no longer simulate v75 programs correctly. Let's fix that by
    re-introducing the v75 semantics and letting QEMU use the right version
    depending on the running revision. For now, the instruction descriptions
    and attributes are always set to the latest revision, for convenience.
    
    Signed-off-by: Matheus Tavares Bernardino <quic_mathbern@quicinc.com>
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

---
+++ target/hexagon/gen_mmvec_ext.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+import sys
+from collections import namedtuple
+
+Q6INSN_data = namedtuple('Q6INSN_data', 'beh,attribs,descr,sem')
+
+data = {}
+def Q6INSN_REV(rev, tag, beh, attribs, descr, sem):
+    global data
+    if tag not in data:
+        data[tag] = {}
+    data[tag][rev] = Q6INSN_data(beh, attribs, descr, sem)
+
+def read_preprocessed_file(name):
+    for line in open(name, "rt").readlines():
+        if not line.startswith("#"):
+            for insn in line.split("Q6INSN_REV_END"):
+                insn = insn.strip()
+                if insn != '':
+                    eval(insn)
+
+def write_to_output(name):
+    with open(name, "w") as out:
+        for tag, datum in data.items():
+            revs = sorted(datum.keys(), reverse=True)
+            latest_rev_datum = datum[revs[0]]
+            sem = "do { "
+            for i, rev in enumerate(revs):
+                if i < len(revs) - 1:
+                    sem += (
+                        f"if (HEXAGON_REV_BYTE() >= 0x{rev}) "
+                        f"{{ {datum[rev].sem}; }} else "
+                    )
+                else:
+                    sem += f"{{ {datum[rev].sem}; }};"
+            sem += " } while (0)"
+            out.write(f"Q6INSN({tag}, \"{latest_rev_datum.beh}\"," +
+                      f" {latest_rev_datum.attribs}," +
+                      f" \"{latest_rev_datum.descr}\", {sem})\n")
+
+def main():
+    read_preprocessed_file(sys.argv[1])
+    write_to_output(sys.argv[2])
+
+if __name__ == "__main__":
+    main()
