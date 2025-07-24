6a7a7bf47c24856b215235f0cf8bac0663219a2a
Brian Cain <brian.cain@oss.qualcomm.com>
2025-07-24 12:50:12 -0700

    fixup! draft: QOM-ify system regs

--- target/hexagon/cpu.h
+++ target/hexagon/cpu.h
@@ -496,8 +496,6 @@ struct ArchCPU {
     uint32_t vmstate_num_g_sreg;
     uint32_t vmstate_num_g_gcycle;
     uint32_t hvx_contexts;
-    uint32_t boot_addr;
-    uint64_t config_table_addr;
 
     struct HexagonSysregState *sysregs;
 #endif
