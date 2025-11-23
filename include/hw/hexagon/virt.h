b63088e83110bf6099a2bbb481486247dbe1a1f4
Brian Cain <brian.cain@oss.qualcomm.com>
2025-11-23 17:58:23 -0600

    hw/hexagon/virt: add gsregs to HexagonVirtMachineState
    Add gsregs field to HexagonVirtMachineState and change all references
    in virt.c to use vms->gsregs instead of gsregs_dev.
    
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

--- include/hw/hexagon/virt.h
+++ include/hw/hexagon/virt.h
@@ -34,6 +34,7 @@ struct HexagonVirtMachineState {
     MemoryRegion vtcm;
     MemoryRegion bios;
     DeviceState *l2vic;
+    DeviceState *gsregs;
     Clock *apb_pclk;
     HexagonBootInfo bootinfo;
     QCTQtimerState *qtimer;
