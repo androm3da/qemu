4472b71bf7ea3afa97b42c4e55fede51ed2c63ee
Matheus Tavares Bernardino <matheus.bernardino@oss.qualcomm.com>
2026-04-16 04:38:52 -0700

    target/hexagon/cpu: add HVX IEEE FP extension
    This flag will be used to control the HVX IEEE float instructions, which
    are only available at some Hexagon cores. When unavailable, the
    instruction effectively only set the destination registers to 0.
    
    Signed-off-by: Matheus Tavares Bernardino <matheus.bernardino@oss.qualcomm.com>
    Reviewed-by: Brian Cain <brian.cain@oss.qualcomm.com>

--- target/hexagon/translate.c
+++ target/hexagon/translate.c
@@ -1192,6 +1192,7 @@ static void hexagon_tr_init_disas_context(DisasContextBase *dcbase,
     ctx->branch_cond = TCG_COND_NEVER;
     ctx->is_tight_loop = FIELD_EX32(hex_flags, TB_FLAGS, IS_TIGHT_LOOP);
     ctx->short_circuit = hex_cpu->short_circuit;
+    ctx->ieee_fp_extension = hex_cpu->ieee_fp_extension;
     ctx->hex_def = HEXAGON_CPU_GET_CLASS(hex_cpu)->hex_def;
 #ifndef CONFIG_USER_ONLY
     ctx->num_cycles = 0;
