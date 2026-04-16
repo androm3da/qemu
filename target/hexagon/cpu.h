ce8fa6d9ff9f4938d85dec44d25b8b1cd7380673
Matheus Tavares Bernardino <matheus.bernardino@oss.qualcomm.com>
2026-04-16 04:38:53 -0700

    hexagon: group cpu configurations in their own struct
    This will be used in a follow up commit.
    
    Reviewed-by: Taylor Simpson <ltaylorsimpson@gmail.com>
    Signed-off-by: Matheus Tavares Bernardino <matheus.bernardino@oss.qualcomm.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Brian Cain <brian.cain@oss.qualcomm.com>

--- target/hexagon/cpu.h
+++ target/hexagon/cpu.h
@@ -185,15 +185,13 @@ typedef struct HexagonCPUClass {
     const HexagonCPUDef *hex_def;
 } HexagonCPUClass;
 
+#include "cpu_bits.h"
+
 struct ArchCPU {
     CPUState parent_obj;
 
     CPUHexagonState env;
-
-    bool lldb_compat;
-    target_ulong lldb_stack_adjust;
-    bool short_circuit;
-    bool ieee_fp_extension;
+    HexagonCPUConfig cfg;
 #ifndef CONFIG_USER_ONLY
     HexagonTLBState *tlb;
     uint32_t boot_addr;
@@ -201,8 +199,6 @@ struct ArchCPU {
     uint32_t htid;
 #endif
 };
-
-#include "cpu_bits.h"
 
 FIELD(TB_FLAGS, IS_TIGHT_LOOP, 0, 1)
 FIELD(TB_FLAGS, MMU_INDEX, 1, 3)
